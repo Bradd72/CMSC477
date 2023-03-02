8329e29bb12de880b37c49403587d4fdb355297a
Jack Mirenzi <jmirenzi717@gmail.com>
2023-03-02 13:56:51 -0500

    Localization Working and started adding pid

--- Lab_1/movement_testing.py
+++ Lab_1/movement_testing.py
@@ -68,7 +68,19 @@ def find_pose_from_tag(K, detection):
 
 if __name__ == '__main__':
     np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+    ep_robot = robot.Robot()
+    ep_robot.initialize(conn_type="ap")
+    ep_chassis = ep_robot.chassis
 
+    x_maze_des = 45
+    y_maze_des = 45
+    K_p = 1.5
+    K_i = .15
+    K_d = .1
+    robot_coord=[0,0]
+    best_globe_to_bot_trans=np.eye(4)
+    prog_time=time.time()
+    time_=prog_time
 
     ep_robot = robot.Robot()
     ep_robot.initialize(conn_type="ap")
@@ -82,15 +94,12 @@ if __name__ == '__main__':
             img = ep_camera.read_cv2_image(strategy="newest", timeout=0.5)   
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             gray.astype(np.uint8)
-
             K=np.array([[184.752, 0, 320], [0, 184.752, 180], [0, 0, 1]])
-
             results = at_detector.detect(gray, estimate_tag_pose=False)
-
+            best_weight=0
             x_coord=[]
-            x_weights=[]
             z_coord=[]
-            z_weights=[]
+            weights=[]
             for res in results:
                 pose = find_pose_from_tag(K, res)
                 pts = res.corners.reshape((-1, 1, 2)).astype(np.int32)
@@ -102,26 +111,30 @@ if __name__ == '__main__':
                 img = cv2.polylines(img, [pts], isClosed=True, color=(0, 255, 0), thickness=5)
                 cv2.circle(img, tuple(res.center.astype(np.int32)), 5, (0, 0, 255), -1)
                 bot_to_tag_trans = pose_transform(pose)
-                
                 for i in range(len(marker_List)):
                     if marker_List[i][0] == res.tag_id:
                         globe_to_tag_trans = marker_transform(marker_List[i,1],marker_List[i,2],1*(marker_List[i,3]))
                         globe_to_bot_trans = np.matmul(globe_to_tag_trans,bot_to_tag_trans)
-
+                        weight=abs(bot_to_tag_trans[0,3])**-1
+                        if weight>best_weight:
+                            best_globe_to_bot_trans=globe_to_bot_trans
                         x_coord.append(globe_to_bot_trans[0,3])
-                        x_weights.append(abs(bot_to_tag_trans[0,3])**-1)
-                        
                         z_coord.append(globe_to_bot_trans[2,3])
-                        z_weights.append(abs(bot_to_tag_trans[0,3])**-1)
+                        weights.append(weight)
                         # print("{}: {} {} ({}, {})".format(res.tag_id,pose[0]*METERS_TO_MAZE,np.rad2deg(pose[1]),marker_List[i][1],marker_List[i][2]))
                 
-                
+               
         
             if x_coord != []:
-                robot_coord = [np.average(x_coord,weights=x_weights),np.average(z_coord,weights=z_weights)]
-            # TODO: figure out why the samples give such bad estimation
-            # print("{} {} {}".format(res.tag_id,pose[0],pose[1]))
+                robot_coord = [np.average(x_coord,weights=weights),np.average(z_coord,weights=weights)]
             print("{:.3f},{:.3f}    {:.2f},{:.2f}   {:.2f} {:.2f}".format(robot_coord[0],robot_coord[1],robot_coord[0]*METERS_TO_MAZE,robot_coord[1]*METERS_TO_MAZE,np.var(z_coord),np.var(x_coord)))
+            index_best = max(range(len(weights)), key=weights.__getitem__)
+            ##TODO use best_globe_to_bot_trans to convert robot_coord into x and y of the robot
+            x_error=x_maze_des*MAZE_TO_METERS-robot_coord[0]
+            z_error=y_maze_des*MAZE_TO_METERS-robot_coord[1]
+            x_response=K_p*x_error
+            z_response=K_p*z_error
+            ep_chassis.drive_speed(x_response, z_response, 0,timeout=.1)
 
             cv2.imshow("img", img)
             cv2.waitKey(10)
