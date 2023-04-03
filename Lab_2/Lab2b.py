91f827ec19d48eaecce44fa611a74410ae94b94a
Jack Mirenzi <jmirenzi717@gmail.com>
2023-04-03 00:15:07 -0400

    created the outline of the final code

--- Lab_2/Lab2b.py
+++ Lab_2/Lab2b.py
@@ -34,10 +34,6 @@ error_count_head=0
 prev_time=time.time()
 prev_time_head=time.time()
 
-def sub_data_handler(sub_info):
-    global pos_x, pos_y 
-    pos_x, pos_y = sub_info
-
 def centroid_pid(des_cX,des_cY):
     global y_diff,x_diff,y_integrator,x_integrator,y_error,x_error,y_response,x_response,error_count,error_norm,prev_time
     y_prev=y_error
@@ -105,7 +101,6 @@ if __name__ == '__main__':
     ep_gripper.open()
     ep_arm.moveto(180,-20).wait_for_completed()
     
-    ep_arm.sub_position(freq=5, callback=sub_data_handler)
     ep_camera.start_video_stream(display=False, resolution=camera.STREAM_360P)
     run_bool=True
     time.sleep(2)
@@ -119,33 +114,18 @@ if __name__ == '__main__':
 
     des_cX=320
     des_cY=200
-    item_found = False
-    goal="lego"
+    goal="river"
     while(run_bool):
         frame = ep_camera.read_cv2_image(strategy="newest", timeout=0.5)   
         frame_height, frame_width, c = frame.shape
         output = frame.copy() 
+        
+        
+     
 
         ## Find Block from Components
         item_found = False
-        if goal=='lego':
-            fps_time = time.time()
-            preds = model.predict(frame, confidence=40, overlap=30).json()['predictions']
-            for pred in preds:
-                if pred['class'] == 'lego':
-                    item_found = True
-                    x1=round(pred['x']-pred['width']/2)
-                    y1=round(pred['y']-pred['height']/2)
-                    x2=round(pred['x']+pred['width']/2)
-                    y2=round(pred['y']+pred['height']/2)
-                    mask=np.zeros((frame_height, frame_width))
-                    mask[x1:x2][y1:y2] = 1
-                    cX=pred['x'];cY=pred['y']
-                    cv2.circle(output, (int(cX), int(cY)), 4, (0, 0, 255), -1)
-                    cv2.rectangle(img=output, pt1=(x1,y1),pt2=(x2,y2),color=(0,255,255), thickness=2)
-            fps = 1.0/(time.time()-fps_time)
-        elif goal=='river':
-            ## Image Processing
+        if goal=='river':
             blurred = cv2.medianBlur(frame,9)
             hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
             mask = cv2.inRange(hsv, lower_blue, upper_blue)
@@ -169,35 +149,72 @@ if __name__ == '__main__':
                         if mask[y+j,x+x_shift]:
                             dy1=j
                     cv2.line(output, (x,y+dy1), (x+w, y+dy2), (0, 0, 255),2)
-                    (cX, cY) = centroid[i]
+                    (cX_, cY) = centroid[i]
                     cv2.circle(output, (int(cX), int(cY)), 4, (0, 0, 255), -1)
                     heading=np.arctan2(dy2-dy1,w-2*x_shift)
+            # Lego tracking left to right
+            fps_time = time.time()
+            preds = model.predict(frame, confidence=40, overlap=30).json()['predictions']
+            for pred in preds:
+                if pred['class'] == 'lego':
+                    item_found = True
+                    x1=round(pred['x']-pred['width']/2)
+                    y1=round(pred['y']-pred['height']/2)
+                    x2=round(pred['x']+pred['width']/2)
+                    y2=round(pred['y']+pred['height']/2)
+                    mask=np.zeros((frame_height, frame_width))
+                    mask[x1:x2][y1:y2] = 1
+                    cX=pred['x']
+                    cY_lego=pred['y']
+                    cv2.circle(output, (int(cX), int(cY)), 4, (0, 0, 255), -1)
+                    cv2.rectangle(img=output, pt1=(x1,y1),pt2=(x2,y2),color=(0,255,255), thickness=2)
+            fps = 1.0/(time.time()-fps_time)
+        elif goal=='orange':
+            blurred = cv2.medianBlur(frame,9)
+            hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+            mask = cv2.inRange(hsv, lower_org, upper_org)
+            analysis = cv2.connectedComponentsWithStats(mask,4,cv2.CV_32S)
+            (totalLabels, label_ids, values, centroid) = analysis  
+            x_shift=10
+            for i in range(1, totalLabels):
+                area = values[i, cv2.CC_STAT_AREA] 
+                # print("area: {}  w:{} h:{}".format(area,w,h))
+                # Checks if Item is big enough and not at the top of screen
+                if (area > 1000) and (area < 10000) and (y>60) and (w/h>2):
+                    item_found = True
+                    componentMask = (label_ids == i).astype("uint8") * 255
+                    for j in range(h-1,0,-1):
+                        if mask[y+j,x+w-x_shift]:
+                            dy2=j
+                        if mask[y+j,x+x_shift]:
+                            dy1=j
+                    cv2.line(output, (x,y+dy1), (x+w, y+dy2), (0, 0, 255),2)
+                    (cX, cY) = centroid[i]
+                    cv2.circle(output, (int(cX), int(cY)), 4, (0, 0, 255), -1)
 
         ## Arm Controller
         # print("Robotic Arm: pos x:{0}, pos y:{1}".format(pos_x, pos_y))
         if item_found:
             if goal=='lego':
-                if centroid_pid(des_cX=des_cX,des_cY=des_cY):
-                    if des_cY == 200:
-                        des_cY=320
-                    else:
-                        ep_chassis.drive_speed(0,0,0)
-                        time.sleep(.1)
-                        ep_gripper.close()
-                        time.sleep(2)
-                        ep_gripper.stop()
-                        ep_arm.move(0,50).wait_for_completed() 
-                        # ep_arm.move(-10,0).wait_for_completed() #doesn't do anything  
-                        goal='river'
-                z_response=0
-            elif goal=='river':
-                if centroid_pid(320,200) and heading_pid(0):
+                # TODO: perfect this part
+                if centroid_pid(des_cX=des_cX,des_cY=des_cY) and heading_pid(0) and cY_lego>200:
+                    ep_chassis.drive_speed(0,0,0)
+                    ep_gripper.open()
+                    time.sleep(1)
                     ep_chassis.drive_speed(.3,0,0)
                     time.sleep(.8)
                     ep_chassis.drive_speed(0,0,0)
-                    time.sleep(5) # buffer for other robot to get in position
-                    break
-                    # TODO: fix speed and sleep values
+                    ep_gripper.close()
+                    ep_chassis.drive_speed(-.5,0,0)
+                    time.sleep(1)
+                    goal=='orange'
+            elif goal=='orange':
+                    if centroid_pid(200,300):
+                        ep_chassis.drive_speed(.3,-.3,0)
+                        time.sleep(.85)
+                        ep_chassis.drive_speed(0,0,0)
+                        ep_gripper.open()
+                        break
             ep_chassis.drive_speed(x_response, y_response,z_response,timeout=.5)
             print("({}, {}) x_response:{:.2f},  y_response: {:.2f},  error_norm: {:.2f},  z_response: {:.2f}, fps: {:.1f}".format(des_cX,des_cY,x_response,y_response,np.mean(error_norm),z_response,fps))
         else:
